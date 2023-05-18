aa21b8cca34747b80387439e023d8d681adf0bf8
tolgakb <tolgakabaahmet@gmail.com>
2023-05-18 22:31:33 +0100

    Index and detail page has been created. Pagination feature has been implemented. Products can be searched in terms of their title  and listed at index page.

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
+from .models import Product
+from django.core.paginator import Paginator
 # Create your views here.
+
+def index(request):
+    products= Product.objects.all()
+
+
+    #search code
+    item_name = request.GET.get('item_name')
+
+    if item_name != '' and item_name is not None:
+        products = products.filter(title__icontains= item_name)
+
+    #paginator code
+    paginator = Paginator(products, 4)
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'shop/index.html', context)
+
+
+def detail(request, id):
+
+    product_object = Product.objects.get(id = id)
+
+    context = {
+        'product_object': product_object,
+    }
+    return render(request, 'shop/detail.html', context)
